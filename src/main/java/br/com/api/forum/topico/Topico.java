a345ad1196e6de3f1ab1e7fb7e2a575b875ef5f6
Henriquemendes1 <henrique.mend258@gmail.com>
2024-06-27 14:39:16 -0300

    Initial commit

---
+++ src/main/java/br/com/api/forum/topico/Topico.java
@@ -0,0 +1,11 @@
+package br.com.api.forum.topico;
+
+public class Topico {
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private Double dataDeCriacao;
+    private String estadoDoTopico;
+    private String autor;
+    private String curso;
+}
